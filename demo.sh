d0e7c974329addd98ee6d9de8119f87d13bef1bd
Paul Ruvolo <paullundyruvolo@gmail.com>
2021-12-02 12:13:48 -0500

    fix demo scripts

--- demo.sh
+++ demo.sh
@@ -2,7 +2,9 @@
 
 IMAGE_IDX=0003
 
+mkdir output_sample
+
 eog /home/powerhorse/Documents/visualAlignmentComparison/1634152465.40384/${IMAGE_IDX}.jpg &
 
-./build/visual_alignment --align-image-path ~/Documents/visualAlignmentComparison/1634152465.40384/alignimage_${IMAGE_IDX}.jpg --align-metadata-path ~/Documents/visualAlignmentComparison/1634152465.40384/alignimage_${IMAGE_IDX}_metadata.json --camera-image-path ~/Documents/visualAlignmentComparison/1634152465.40384/cameraimage_${IMAGE_IDX}.jpg --camera-metadata-path ~/Documents/visualAlignmentComparison/1634152465.40384/cameraimage_${IMAGE_IDX}_metadata.json --vftype 3 --output-directory output_sample --debug true
+./build/visual_alignment --align-image-path ~powerhorse/Documents/visualAlignmentComparison/1634152465.40384/alignimage_${IMAGE_IDX}.jpg --align-metadata-path ~powerhorse/Documents/visualAlignmentComparison/1634152465.40384/alignimage_${IMAGE_IDX}_metadata.json --camera-image-path ~powerhorse/Documents/visualAlignmentComparison/1634152465.40384/cameraimage_${IMAGE_IDX}.jpg --camera-metadata-path ~powerhorse/Documents/visualAlignmentComparison/1634152465.40384/cameraimage_${IMAGE_IDX}_metadata.json --vftype 3 --output-directory output_sample --debug false
 
--- demo_sfm.sh
+++ demo_sfm.sh
@@ -2,5 +2,7 @@
 
 IMAGE_IDX=0019
 
-./build/sfm --image-1-path ~/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_0001.jpg --metadata-1-path ~/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_0001_metadata.json --image-2-path ~/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_${IMAGE_IDX}.jpg --metadata-2-path ~/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_${IMAGE_IDX}_metadata.json --vftype 4 --output-directory output_sfm --ratio-test 0.4
+mkdir output_sfm
+
+./build/sfm --image-1-path ~powerhorse/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_0001.jpg --metadata-1-path ~powerhorse/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_0001_metadata.json --image-2-path ~powerhorse/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_${IMAGE_IDX}.jpg --metadata-2-path ~powerhorse/Documents/visualAlignmentComparison/1635895514.298703/cameraimage_${IMAGE_IDX}_metadata.json --vftype 4 --output-directory output_sfm --ratio-test 0.4
 
